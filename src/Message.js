8984c8f2c4ab953db6ebb43039685c40b5e83bfc
nikson1701 <78842306+nikson1701@users.noreply.github.com>
2021-08-31 11:42:03 +0300

    HW #2
    Сделано на функциональных и на объектных компонентах (реализация на функциональных закомментирована)
    - Добавлен стейт messageList, в котором хранится массив объектов - сообщений
    - Также в стейте messageText и messageAuthor для формы
    - Список сообщений рендерится в компоненте Message через map.
    - Добавлена форма с полями для ввода текста, имени и кнопкой отправки сообщения.
    - На каждое отправленное пользователем сообщение с задержкой отвечает робот

---
+++ src/Message.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Message.css';
+/*
+function Message(props) {
+  return (
+    props.messages.map((message, i) => <article key={i} className="message"><h2 className="massage-header">{message.header}</h2><p className="message-text">{message.text}</p><p className="message-author"><cite>{message.author}</cite></p></article>)
+  );
+}
+*/
+
+class Message extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(
+			this.props.messages.map((message, i) => <article key={i} className="message"><h2 className="massage-header">{message.header}</h2><p className="message-text">{message.text}</p><p className="message-author"><cite>{message.author}</cite></p></article>)
+		);
+	}
+}
+
+export default Message;
