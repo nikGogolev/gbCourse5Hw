d2518f157c8380a4124f2d73fcca1cb6fbfbae79
nikson1701 <78842306+nikson1701@users.noreply.github.com>
2021-09-29 14:44:59 +0300

    Add files via upload

--- src/store/sagas/__tests__/newsSaga.test.js
+++ src/store/sagas/__tests__/newsSaga.test.js
@@ -4,27 +4,36 @@ import { combine, store } from '../../store';
 import initialState from '../../initialState';
 
 import { put, call, take } from 'redux-saga/effects';
-import { testSaga } from 'redux-saga-test-plan';
+import { expectSaga, testSaga } from 'redux-saga-test-plan';
+import { URL_NEWS } from '../../../utils/constants';
+import { getNewsFailure, getNewsRequest, getNewsSuccess } from '../../actions/getNews';
 
 const{fetchData, onGetNewsWithSaga} = newsSaga;
 
 
+// test('', () => {
+//     return testSaga(onGetNewsWithSaga)
+//     .next()
+//     .put(getNewsRequest())
+//     .next()
+//     .call(fetchData, URL_NEWS);
+// });
+const result = {news:'123'};
+it('returns good result of fetch', () => {
+    return expectSaga(onGetNewsWithSaga)
+    .provide([
+        [call(fetchData, URL_NEWS), result]
+    ])
+    .put(getNewsRequest())
+    .call(fetchData, URL_NEWS)
+    .put(getNewsSuccess(result.news))
+    .run();
+});
 
-describe('', () => {
-    it('', async () => {
-        fetchMock.mockOnce(JSON.stringify({news:'123'}));
-        const gen = onGetNewsWithSaga();
-
-        const result = await gen.next();
-        expect(result.value.payload.action).toEqual({ type: 'NEWS::GET_NEWS_REQUEST' });
-        
-        const result1 = await gen.next().value.payload;
-        //const data = await result1.fn(result1.args[0]);
-        console.log(result1);
-        expect(result.value.payload.action).toEqual({ type: 'NEWS::GET_NEWS_REQUEST' });
-        
-        const result2 = await gen.next();
-        console.log(result2.value);
-        expect(result.value.payload.action).toEqual({ type: 'NEWS::GET_NEWS_REQUEST' });
-    });
+it('returns error in fetch', () => {
+    return expectSaga(onGetNewsWithSaga)
+    .put(getNewsRequest())
+    .call(fetchData, URL_NEWS)
+    .put(getNewsFailure("Cannot read property 'then' of undefined"))
+    .run();
 });
